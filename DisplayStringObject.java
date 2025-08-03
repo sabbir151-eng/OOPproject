81829aa8423874b768521a23df9ce42495e17a3d
sabbir151-eng <abdulsabbir9040@gmail.com>
2025-08-03 19:02:09 +0530

    Add files via upload

---
+++ DisplayStringObject.java
@@ -0,0 +1,12 @@
+package strings;
+import java.util.Scanner;
+
+
+public class DisplayStringObject {
+	public static void main(String[] args) {
+        String message = "Hello, World! This is a string object.";
+        
+        System.out.println(message);
+    }
+
+}
---
+++ ReadAndPrintString.java
@@ -0,0 +1,16 @@
+package strings;
+import java.util.Scanner;
+
+public class ReadAndPrintString {
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        System.out.print("Enter a string: ");
+	        
+	        String userInput = scanner.nextLine();
+	        
+	        System.out.println("You entered: " + userInput);
+	        
+	        scanner.close();
+	    }
+}
